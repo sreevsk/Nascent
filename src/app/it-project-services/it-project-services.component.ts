cbff5254305daccc9e026758ed7c2553c2561fd6
svoma <vomasrikanth5209@gmail.com>
2023-08-11 00:50:54 +0530

    CSS adjustments

--- src/app/it-project-services/it-project-services.component.ts
+++ src/app/it-project-services/it-project-services.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-it-project-services',
   templateUrl: './it-project-services.component.html',
-  styleUrls: ['./it-project-services.component.css']
+  styleUrls: ['./it-project-services.component.scss']
 })
 export class ItProjectServicesComponent implements OnInit {
 
--- src/app/our-jobs/our-jobs.component.ts
+++ src/app/our-jobs/our-jobs.component.ts
@@ -9,11 +9,20 @@ import { SharedDataService } from '../shared-data.service';
   styleUrls: ['./our-jobs.component.css']
 })
 export class OurJobsComponent implements OnInit {
-
+  searchText: string = '';
   jobItems: any;
+  unfilteredJobItems: any;
   customArray: number[] = [];
+  jobCategories: any;
+  jobTypes: any;
+  jobLocations: any;
+
   constructor(private sharedDataService: SharedDataService) {
-    this.jobItems = sharedDataService.getJobPageData().filter(item => item["available"] == true);
+    this.unfilteredJobItems = sharedDataService.getJobPageData().filter(item => item["available"] == true)
+    this.jobItems = [...this.unfilteredJobItems];
+    this.jobCategories = new Set(this.jobItems.map(obj => obj["jobCategory"]));
+    this.jobTypes = new Set(this.jobItems.map(obj => obj["type"]));
+    this.jobLocations = new Set(this.jobItems.map(obj => obj["location"]));
     this.generateCustomArray(this.jobItems.length, 3);
   }
 
@@ -21,9 +30,20 @@ export class OurJobsComponent implements OnInit {
   }
 
   generateCustomArray(length: number, increment: number): void {
+    this.customArray = [];
     for (let i = 0; i < length; i = i + increment) {
       this.customArray.push(i);
     }
+  }
+
+  filter(text: string, filterText: string) {
+    this.jobItems = this.unfilteredJobItems.filter(obj => obj[filterText] === text);
+    this.generateCustomArray(this.jobItems.length, 3);
+  }
+
+  onSearch() {
+    this.jobItems = this.unfilteredJobItems.filter(obj => obj["title"].toUpperCase().includes(this.searchText.toUpperCase()));
+    this.generateCustomArray(this.jobItems.length, 3);
   }
 
   getJobId(job: any) {
