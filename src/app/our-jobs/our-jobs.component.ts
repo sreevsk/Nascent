4b8d7e1d499e0424cd05f160f3dcdd50fe3694e9
svoma <vomasrikanth5209@gmail.com>
2023-08-14 22:24:31 +0530

    FIlter changes in our jobs

--- src/app/our-jobs/our-jobs.component.ts
+++ src/app/our-jobs/our-jobs.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
 import { SharedDataService } from '../shared-data.service';
 
 
 @Component({
   selector: 'app-our-jobs',
   templateUrl: './our-jobs.component.html',
-  styleUrls: ['./our-jobs.component.css']
+  styleUrls: ['./our-jobs.component.scss'],
 })
-export class OurJobsComponent implements OnInit {
+export class OurJobsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('jobCategory', { static: false }) dropDown: ElementRef;
+  // @ViewChild('jobType', { static: false }) jobType: ElementRef;
+  // @ViewChild('jobLocation', { static: false }) jobLocation: ElementRef;
+
+
   searchText: string = '';
   jobItems: any;
   unfilteredJobItems: any;
@@ -17,13 +22,32 @@ export class OurJobsComponent implements OnInit {
   jobTypes: any;
   jobLocations: any;
 
-  constructor(private sharedDataService: SharedDataService) {
+
+  jobCategoryDefaultTitle: string = "All Job Categories";
+  jobTypeDefaultTitle: string = "All Job Types";
+  jobLocationDefaultTitle: string = "All Job Locations";
+  selectedJobCategory = this.jobCategoryDefaultTitle;
+  selectedJobType = this.jobTypeDefaultTitle;
+  selectedJobLocation = this.jobLocationDefaultTitle;
+
+
+  constructor(private sharedDataService: SharedDataService, private renderer: Renderer2) {
     this.unfilteredJobItems = sharedDataService.getJobPageData().filter(item => item["available"] == true)
     this.jobItems = [...this.unfilteredJobItems];
-    this.jobCategories = new Set(this.jobItems.map(obj => obj["jobCategory"]));
-    this.jobTypes = new Set(this.jobItems.map(obj => obj["type"]));
-    this.jobLocations = new Set(this.jobItems.map(obj => obj["location"]));
+
+    this.jobCategories = this.populateSetWithInitialValue(this.jobCategoryDefaultTitle, this.jobItems.map(obj => obj["jobCategory"]));
+    this.jobTypes = this.populateSetWithInitialValue(this.jobTypeDefaultTitle, this.jobItems.map(obj => obj["type"]));
+    this.jobLocations = this.populateSetWithInitialValue(this.jobLocationDefaultTitle, this.jobItems.map(obj => obj["location"]));
+
     this.generateCustomArray(this.jobItems.length, 3);
+  }
+
+
+  populateSetWithInitialValue(value: string, valueArr) {
+    const mySet = new Set();
+    mySet.add(value)
+    valueArr.forEach(value => mySet.add(value));
+    return mySet;
   }
 
   ngOnInit(): void {
@@ -36,13 +60,31 @@ export class OurJobsComponent implements OnInit {
     }
   }
 
-  filter(text: string, filterText: string) {
-    this.jobItems = this.unfilteredJobItems.filter(obj => obj[filterText] === text);
-    this.generateCustomArray(this.jobItems.length, 3);
+  ngAfterViewInit() {
   }
 
-  onSearch() {
-    this.jobItems = this.unfilteredJobItems.filter(obj => obj["title"].toUpperCase().includes(this.searchText.toUpperCase()));
+  cascadeButtonData(text) {
+    if (this.dropDown) {
+      const elementWidth = this.dropDown.nativeElement.offsetWidth;
+      return text.length * 10 > elementWidth ? text.substring(0, 18) + ".." : text;
+    }
+  }
+
+  clearSearch() {
+    this.searchText = '';
+    this.selectedJobCategory = this.jobCategoryDefaultTitle;
+    this.selectedJobLocation = this.jobLocationDefaultTitle;
+    this.selectedJobType = this.jobTypeDefaultTitle;
+    this.filter();
+  }
+
+  filter() {
+    this.jobItems = this.unfilteredJobItems
+      .filter(obj =>
+        (this.searchText == undefined || this.searchText === '' || obj['title'].toUpperCase().includes(this.searchText.toUpperCase())) &&
+        (this.jobCategoryDefaultTitle === this.selectedJobCategory || obj['jobCategory'] === this.selectedJobCategory) &&
+        (this.jobTypeDefaultTitle === this.selectedJobType || obj['type'] === this.selectedJobType) &&
+        (this.jobLocationDefaultTitle === this.selectedJobLocation || obj['location'] === this.selectedJobLocation));
     this.generateCustomArray(this.jobItems.length, 3);
   }
 
